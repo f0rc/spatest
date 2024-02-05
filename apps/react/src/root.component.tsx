a9570490b07b8dd4575d1f1ce7ab7c8e8e4edd22
f0rc <brickmp4@gmail.com>
2024-02-04 22:30:59 -0600

    angular live reload css/tailwind

--- apps/react/src/root.component.tsx
+++ apps/react/src/root.component.tsx
@@ -3,7 +3,7 @@ import Parcel from "single-spa-react/parcel";
 
 export default function Root(props) {
   return (
-    <div className="bg-blue-600">
+    <div className="bg-blue-600 border-4 border-black">
       <div>{props.name} is mounted!</div>
 
       <Parcel
